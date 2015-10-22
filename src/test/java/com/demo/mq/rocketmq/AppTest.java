9c396013b7a39eb210c6fdd643d5f37c25a1e4a3
岳志华 <yuezhihua@joyworks.com.cn>
2015-10-22 11:39:19 +0800

    初始化mq-demo

---
+++ src/test/java/com/demo/mq/rocketmq/AppTest.java
@@ -0,0 +1,18 @@
+package com.demo.mq.rocketmq;
+
+import org.junit.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    
+    
+    @Test
+    public void test1(){
+        
+    }
+    
+    
+}
